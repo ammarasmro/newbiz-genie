890dd60f032a7631246d817525f470e8a5c4d89b
Ammar Asmro <ammarasmaro@gmail.com>
2021-05-15 15:07:18 -0400

    Add support for seq2seq model in front-end webapp

--- app/run.py
+++ app/run.py
@@ -1,6 +1,9 @@
-import streamlit as st
 import spacy
+import streamlit as st
+import torch
+from name_generator.models.encoder_decoder_model import EncoderDecoderModel
 from name_generator.models.ly_model import LyModel
+from name_generator.models.nn_models import AttnDecoderRNN, EncoderRNN
 
 st.title('Business Namer')
 
@@ -11,6 +14,7 @@ st.write(description)
 
 model_options = [
     '-ly model',
+    'seq-to-char model'
 ]
 
 model_name = st.selectbox(
@@ -20,9 +24,13 @@ model_name = st.selectbox(
 if model_name == model_options[0]:
     model = LyModel()
     nlp = spacy.load("en_core_web_sm")
+elif model_name == model_options[1]:
+    model = EncoderDecoderModel()
 
 if st.button('button'):
     if model_name == model_options[0]:
         doc = nlp(description)
         for chunk in doc.noun_chunks:
             st.write(model.predict(chunk.lemma_.split(' ')[0]).title())
+    elif model_name == model_options[1]:
+        st.write(model.predict(description))
